aefaf189030cd000fa6fe78a6b0b8681b12b908e
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-08-25 13:23:27 +0200

    fix: added fall safe for config and change routing

--- src/components/RoutingComponent/RoutingComponent.tsx
+++ src/components/RoutingComponent/RoutingComponent.tsx
@@ -1,12 +1,10 @@
-import Home from '@pages/Home/Home';
 import Exams from '@pages/Exams/Exams';
 import { Route, Routes } from 'react-router';
 
 const RoutingComponent = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exams" element={<Exams />} />
+      <Route path="/" element={<Exams />} />
     </Routes>
   );
 };

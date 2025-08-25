f2449f3f49ef3c9dc3cfdcb474384794f256087d
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-08-25 13:22:27 +0200

    fix: Changed ExamCard and ExamPage so its more fitting to the name

--- src/components/ExamCard/ExamCard.tsx
+++ src/components/ExamCard/ExamCard.tsx
@@ -1,98 +1,76 @@
-import useApi from '@hooks/useApi';
 import { Typography, Card, Box, Divider } from '@mui/joy';
 import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
 import type { Exam } from '@/@custom-types/backendTypes';
 
-const ExamCard = () => {
-  const { requestExams } = useApi();
+interface ExamCardProps {
+  index: number;
+  exam: Exam;
+}
+
+const ExamCard = (props: ExamCardProps) => {
   const { t } = useTranslation();
-  const [requestedExams, setRequestedExams] = useState<Exam[]>([]);
-
-  const fetchExams = async (lecturer: string) => {
-    const exams = await requestExams(lecturer);
-    if (exams) {
-      setRequestedExams(exams);
-    }
-  };
-
-  useEffect(() => {
-    fetchExams('Tom');
-  }, []);
 
   return (
-    <Box
+    <Card
+      key={props.index}
+      color="neutral"
+      variant="outlined"
       sx={{
         display: 'flex',
-        flexWrap: 'wrap',
-        gap: 2,
-        paddingTop: 3,
+        width: 270,
         justifyContent: 'space-around',
+        boxShadow: '1px 1px 0px 1px #d3d3d3',
       }}
     >
-      {requestedExams.map((exam, index) => (
-        <Card
-          key={index}
-          color="neutral"
-          variant="outlined"
-          sx={{
-            display: 'flex',
-            width: 270,
-            justifyContent: 'space-around',
-            boxShadow: '1px 1px 0px 1px #d3d3d3',
-          }}
-        >
-          <Typography level="h3">{exam.name}</Typography>
-          <Divider inset="none" />
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Box>
-              <Typography sx={{ opacity: '50%' }}>
-                {t('pages.exam.module')}
-              </Typography>
-              <Typography level="h3" sx={{ fontSize: 'lg' }}>
-                {exam.module}
-              </Typography>
-            </Box>
-            <Box>
-              <Typography sx={{ opacity: '50%' }}>
-                {t('pages.exam.date')}
-              </Typography>
+      <Typography level="h3">{props.exam.name}</Typography>
+      <Divider inset="none" />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Box>
+          <Typography sx={{ opacity: '50%' }}>
+            {t('pages.exam.module')}
+          </Typography>
+          <Typography level="h3" sx={{ fontSize: 'lg' }}>
+            {props.exam.module}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography sx={{ opacity: '50%' }}>
+            {t('pages.exam.date')}
+          </Typography>
 
-              <Typography>
-                {new Date(exam.date).toLocaleDateString()}
-              </Typography>
-            </Box>
-          </Box>
+          <Typography>
+            {new Date(props.exam.date).toLocaleDateString()}
+          </Typography>
+        </Box>
+      </Box>
 
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Box>
-              <Typography sx={{ opacity: '50%' }}>
-                {t('pages.exam.time')}
-              </Typography>
-              <Typography>{exam.time}</Typography>
-            </Box>
-            <Box>
-              <Typography sx={{ opacity: '50%' }}>
-                {t('pages.exam.exams')}
-              </Typography>
-              <Typography>{exam.submissions}</Typography>
-            </Box>
-          </Box>
-        </Card>
-      ))}
-    </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Box>
+          <Typography sx={{ opacity: '50%' }}>
+            {t('pages.exam.time')}
+          </Typography>
+          <Typography>{props.exam.time}</Typography>
+        </Box>
+        <Box>
+          <Typography sx={{ opacity: '50%' }}>
+            {t('pages.exam.exams')}
+          </Typography>
+          <Typography>{props.exam.submissions}</Typography>
+        </Box>
+      </Box>
+    </Card>
   );
 };
 
--- src/pages/Exams/Exams.tsx
+++ src/pages/Exams/Exams.tsx
@@ -1,7 +1,39 @@
+import type { Exam } from '@/@custom-types/backendTypes';
+import useApi from '@/hooks/useApi';
 import ExamCard from '@components/ExamCard/ExamCard';
+import { Box } from '@mui/joy';
+import { useEffect, useState } from 'react';
 
 const Exams = () => {
-  return <ExamCard />;
+  const [requestedExams, setRequestedExams] = useState<Exam[]>([]);
+  const { requestExams } = useApi();
+
+  const fetchExams = async (lecturer: string) => {
+    const exams = await requestExams(lecturer);
+    if (exams) {
+      setRequestedExams(exams);
+    }
+  };
+
+  useEffect(() => {
+    fetchExams('Tom');
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: 2,
+        paddingTop: 3,
+        justifyContent: 'space-around',
+      }}
+    >
+      {requestedExams.map((exam, index) => (
+        <ExamCard exam={exam} index={index} />
+      ))}
+    </Box>
+  );
 };
 
 export default Exams;
